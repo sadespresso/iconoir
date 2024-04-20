9d18c48b1198dbde661a5c2c5993bdce1e17ca28
Batmend Ganbaatar <batmend@gege.mn>
2024-04-21 02:10:01 +0800

    stroke -> fills, include fonts

--- bin/generate-fonts.sh
+++ bin/generate-fonts.sh
@@ -1,11 +1,12 @@
-[ ! -d ../fonts ] && rm -r ../fonts && mkdir -p ../fonts
+[ -d ../fonts ] && rm -r ../fonts
+mkdir -p ../fonts
 
 cp -R ../icons/ .
 
-reorient ./icons/regular/*.svg
-reorient ./icons/solid/*.svg
+oslllo-svg-fixer -s ./icons/regular -d ./icons/regular
+oslllo-svg-fixer -s ./icons/solid -d ./icons/solid
 
-fantasticon ./icons/regular -o ../fonts -t ttf woff woff2 eot svg -g json -n "Iconoir Regular"
-fantasticon ./icons/solid -o ../fonts -t ttf woff woff2 eot svg -g json -n "Iconoir Solid"
+fantasticon ./icons/regular -o ../fonts -t ttf svg woff woff2 eot -g json -n "Iconoir Regular"
+fantasticon ./icons/solid -o ../fonts -t ttf svg woff woff2 eot -g json -n "Iconoir Solid"
 
 rm -rf ./icons
